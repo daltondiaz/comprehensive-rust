fc695cfda3ee5aab568efa137c72e3c2e2007b83
daltondiaz <d_tecnologia@yahoo.com.br>
2022-12-29 22:57:14 -0300

    chore: ignore target folder

--- day1/implicit-conversions/target/debug/deps/implicit_conversions-242445c50b9257b5.d
+++
@@ -1,5 +0,0 @@
-/home/dalton/Dev/personal/rust/comprehensive-rust/day1/implicit-conversions/target/debug/deps/implicit_conversions-242445c50b9257b5.rmeta: src/main.rs
-
-/home/dalton/Dev/personal/rust/comprehensive-rust/day1/implicit-conversions/target/debug/deps/implicit_conversions-242445c50b9257b5.d: src/main.rs
-
-src/main.rs:
--- day1/implicit-conversions/target/debug/implicit-conversions.d
+++
@@ -1,1 +0,0 @@
-/home/dalton/Dev/personal/rust/comprehensive-rust/day1/implicit-conversions/target/debug/implicit-conversions: /home/dalton/Dev/personal/rust/comprehensive-rust/day1/implicit-conversions/src/main.rs
