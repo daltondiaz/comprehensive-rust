c4602ece6fe1df4ad3eed26786b0fae6318c225d
daltondiaz <d_tecnologia@yahoo.com.br>
2022-12-29 23:27:43 -0300

    exercise: convert types f32, bool and i128 to i16

--- day1/implicit-conversions/src/main.rs
+++ day1/implicit-conversions/src/main.rs
@@ -7,4 +7,14 @@ fn main() {
     let y: i16 = 1000;
 
     println!("{x} * {y} = {}", multiply(x.into(), y));
+
+    let x1: f32 = 12.0;
+    let y1: i128 = 111;
+
+    println!("{x1} * {y1} = {}", multiply(x1 as i16, y1 as i16));
+
+    let x2: bool = true;
+    let y2: bool = false;
+
+    println!("{x2} * {y2} = {}", multiply(x2 as i16, y2 as i16));
 }
